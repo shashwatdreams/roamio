ecbd1f98f1e33f0835af0e618f6809d75584367e
yechancho5 <yechancho5@gmail.com>
2025-01-24 23:54:32 -0500

    Created the MongoDB Model for Crowd Reporting

---
+++ backend/app/models/crowd.py
@@ -0,0 +1,23 @@
+from pymongo import MongoClient
+from datetime import datetime
+
+client = MongoClient("mongodb://localhost:27017/")
+db = client["roamio"]
+crowd_collection = db["crowd_reports"]
+
+class CrowdReport:
+    def __init__(self, station, time, crowd_level, train_id=None, additional_info=None):
+        self.station = station
+        self.time = time
+        self.crowd_level = crowd_level
+        self.train_id = train_id
+        self.additional_info = additional_info
+    
+    def save(self):
+        crowd_collection.insert_one({
+            "station": self.station,
+            "time": self.time,
+            "crowd_level": self.crowd_level,
+            "train_id": self.train_id,
+            "additional_info": self.additional_info,
+        })
