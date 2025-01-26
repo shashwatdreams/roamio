75e2912fa1edf658ad29fe8e4e9417b489ef30ad
yechancho5 <yechancho5@gmail.com>
2025-01-26 07:40:41 -0500

    Fixing CORS

--- backend/app/static/js/app.js
+++ backend/app/static/js/app.js
@@ -1,14 +1,11 @@
-// Define API base URL
-window.API_BASE = "http://localhost:5001/api/crowd";
+window.API_BASE = "http://roamio-nyc-824eaa7752e7.herokuapp.com/api/crowd";
 
-// Define a global fetchData function
 window.fetchData = async function (url) {
   const response = await fetch(url);
   if (!response.ok) throw new Error("Failed to fetch data");
   return await response.json();
 };
 
-// Define a global postData function
 window.postData = async function (url, data) {
   const response = await fetch(url, {
     method: "POST",
