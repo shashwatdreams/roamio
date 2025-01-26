9b4add153f80922fe3d42f1a274ad51e540a58ab
Shashwat Sharma <kush.shashwat@outlook.com>
2025-01-25 21:41:43 -0600

    Prepare app for Heroku

--- backend/app/app.py
+++ backend/app/app.py
@@ -2,25 +2,25 @@ from flask import Flask, send_from_directory, abort
 from flask_cors import CORS
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder=os.path.join(os.path.expanduser("~"), "repos/roamio/frontend"))
 CORS(app, resources={r"/api/*": {"origins": "*"}})  
-
-FRONTEND_DIR = os.path.join(os.path.expanduser("~"), "repos/roamio/frontend")
-
 
 from backend.app.routes.crowd_reporting import crowd_reporting_bp
 app.register_blueprint(crowd_reporting_bp, url_prefix="/api/crowd")
 
 @app.route("/")
 def home():
-    return send_from_directory(FRONTEND_DIR, "index.html")
+    try:
+        return app.send_static_file("index.html")
+    except Exception as e:
+        return f"Error serving index.html: {e}", 500
 
 @app.route("/<path:path>")
 def serve_static_files(path):
     try:
-        return send_from_directory(FRONTEND_DIR, path)
-    except Exception:
-        return abort(404)  
+        return send_from_directory(app.static_folder, path)
+    except Exception as e:
+        return f"Error serving {path}: {e}", 404
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5001)  
+    app.run(debug=True, host="0.0.0.0", port=5001)
